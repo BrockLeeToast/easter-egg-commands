7b948bfc49e3dafd63e08337d4fb4f34c0f1ee52
Strudel2010 <Strudel2010>
2013-12-06 15:32:05 -0800

    Added two new commands: broken and time

--- src/strudel2010/EEC/EEC.java
+++ src/strudel2010/EEC/EEC.java
@@ -63,7 +63,7 @@ public class EEC extends JavaPlugin{
 			player.sendMessage(ChatColor.RED + "#BlameEndain");
 			player.playSound(loc, Sound.ITEM_BREAK, 0.5F, 1F);
 		}
-		if(commandLabel.equalsIgnoreCase("time")) {
+		if(commandLabel.equalsIgnoreCase("time")) { 
 			player.sendMessage(ChatColor.BLUE + "People assume that time is a strict progression of cause to effect," +
 					" but actually from a non-linear, non-subjective viewpoint, it's more like a big ball" +
 					" of wibbly wobbly timey wimey...stuff.");
